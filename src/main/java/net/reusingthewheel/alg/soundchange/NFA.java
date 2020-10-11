a6e1067d88f740cebc2828393276ff2344f537a2
Piotr Rusin <contact@reusingthewheel.net>
2020-10-11 17:33:39 +0200

    Add newSymbolNFA factory method

--- src/main/java/net/reusingthewheel/alg/soundchange/NFA.java
+++ src/main/java/net/reusingthewheel/alg/soundchange/NFA.java
@@ -24,4 +24,18 @@ public class NFA {
 
         return new NFA(start, end);
     }
+
+    /**
+     * Create an automaton that allows for a transition only after consuming the given symbol.
+     *
+     * @param symbol a symbol necessary to be consumed for the automaton to reach its end state.
+     * @return an instance of NFA
+     */
+    public static NFA newSymbolNFA(String symbol) {
+        var start = new State(false);
+        var end = new State(true);
+        start.addSymbolTransitions(symbol, end);
+
+        return new NFA(start, end);
+    }
 }
