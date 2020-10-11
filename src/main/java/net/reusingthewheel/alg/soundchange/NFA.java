33de9987f97d6f01e6e6a4cafa1204ccf7a85f94
Piotr Rusin <contact@reusingthewheel.net>
2020-10-11 17:14:43 +0200

    Add a basic representation of a nondeterministic finite state machine

---
+++ src/main/java/net/reusingthewheel/alg/soundchange/NFA.java
@@ -0,0 +1,14 @@
+package net.reusingthewheel.alg.soundchange;
+
+/**
+ * A nondeterministic finite state automaton
+ */
+public class NFA {
+    private State start;
+    private State end;
+
+    private NFA(State start, State end) {
+        this.start = start;
+        this.end = end;
+    }
+}
