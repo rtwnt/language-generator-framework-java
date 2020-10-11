64c3d956848f3251fefb166dbb5734f90c04dac3
Piotr Rusin <contact@reusingthewheel.net>
2020-10-11 17:37:24 +0200

    Implement creating a union of NFAs

--- src/main/java/net/reusingthewheel/alg/soundchange/NFA.java
+++ src/main/java/net/reusingthewheel/alg/soundchange/NFA.java
@@ -52,4 +52,25 @@ public class NFA {
 
         return new NFA(first.start, second.end);
     }
+
+    /**
+     * Create an automaton that reaches its final state if the first or the second does.
+     *
+     * @param first the first automaton
+     * @param second the second automaton
+     * @return an instance of NFA
+     */
+    public static NFA newUnionNFA(NFA first, NFA second) {
+        var start = new State(false);
+        start.addEmptySymbolTransition(first.start);
+        start.addEmptySymbolTransition(second.start);
+
+        var end = new State(true);
+        first.end.addEmptySymbolTransition(end);
+        first.end.setFinal(false);
+        second.end.addEmptySymbolTransition(end);
+        second.end.setFinal(false);
+
+        return new NFA(start, end);
+    }
 }
