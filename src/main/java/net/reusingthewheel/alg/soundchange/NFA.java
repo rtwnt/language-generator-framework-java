1843a331304e569e67961c05d4f9a0b712f3ba1e
Piotr Rusin <contact@reusingthewheel.net>
2020-10-11 17:32:08 +0200

    Add newEmptySymbolNFA factory method

--- src/main/java/net/reusingthewheel/alg/soundchange/NFA.java
+++ src/main/java/net/reusingthewheel/alg/soundchange/NFA.java
@@ -11,4 +11,17 @@ public class NFA {
         this.start = start;
         this.end = end;
     }
+
+    /**
+     * Create an automaton that allows for a transition without consuming any symbol.
+     *
+     * @return an instance of NFA
+     */
+    public static NFA newEmptySymbolNFA() {
+        var start = new State(false);
+        var end = new State(true);
+        start.addEmptySymbolTransition(end);
+
+        return new NFA(start, end);
+    }
 }
